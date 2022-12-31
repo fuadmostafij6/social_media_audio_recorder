0aff8cba23d507a3bdf26574cd289d963f89cbf3
fuadmostfij6 <fuadmostfij4@gmail.com>
2022-12-31 20:30:01 +0600

    first commit

---
+++ lib/social_media_audio_recorder.dart
@@ -0,0 +1,347 @@
+library social_media_audio_recorder;
+
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_vibrate/flutter_vibrate.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:record/record.dart';
+import 'package:social_media_audio_recorder/widget/Flow.dart';
+import 'package:social_media_audio_recorder/widget/lottie.dart';
+class RecordButton extends StatefulWidget {
+  final AnimationController controller;
+  final double? timerWidth;
+  final double? lockerHeight;
+  final double? size;
+  double? radius;
+  final Color? color;
+  String? filePath;
+  String? documentPath;
+  RecordButton({
+    Key? key,
+    required this.controller, this.timerWidth, this.lockerHeight=200, this.size= 55, this.color=Colors.white,required this.filePath, required this.documentPath, this.radius=10
+  }) : super(key: key);
+
+
+
+  @override
+  State<RecordButton> createState() => _RecordButtonState();
+}
+
+class _RecordButtonState extends State<RecordButton> {
+
+  //
+  // final double lockerHeight = 200;
+  double timerWidth = 0;
+
+  Animation<double>? buttonScaleAnimation;
+  Animation<double>? timerAnimation;
+  Animation<double>? lockerAnimation;
+
+  DateTime? startTime;
+  Timer? timer;
+  String recordDuration = "00:00";
+  Record? record;
+
+  bool isLocked = false;
+  bool showLottie = false;
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    //init();
+    buttonScaleAnimation = Tween<double>(begin: 1, end: 2).animate(
+      CurvedAnimation(
+        parent: widget.controller,
+        curve: const Interval(0.0, 0.6, curve: Curves.elasticInOut),
+      ),
+    );
+    widget.controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    timerWidth =
+    (widget.timerWidth ?? MediaQuery.of(context).size.width - 2 * 8 - 4);
+    timerAnimation =
+        Tween<double>(begin: timerWidth + 8, end: 0)
+            .animate(
+          CurvedAnimation(
+            parent: widget.controller,
+            curve: const Interval(0.2, 1, curve: Curves.easeIn),
+          ),
+        );
+    lockerAnimation =
+        Tween<double>(begin: widget.lockerHeight! + 8, end: 0)
+            .animate(
+          CurvedAnimation(
+            parent: widget.controller,
+            curve: const Interval(0.2, 1, curve: Curves.easeIn),
+          ),
+        );
+  }
+
+  @override
+  void dispose() {
+    record!.dispose();
+    timer?.cancel();
+    timer = null;
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        lockSlider(),
+        cancelSlider(),
+        audioButton(),
+        if (isLocked) timerLocked(),
+      ],
+    );
+  }
+
+  Widget lockSlider() {
+    return Positioned(
+      bottom: -lockerAnimation!.value,
+      child: Container(
+        height: widget.lockerHeight,
+        width: widget.size!,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(widget.radius!),
+          color: widget.color,
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const FaIcon(FontAwesomeIcons.lock, size: 20),
+            const SizedBox(height: 8),
+            FlowShader(
+              direction: Axis.vertical,
+              child: Column(
+                children: const [
+                  Icon(Icons.keyboard_arrow_up),
+                  Icon(Icons.keyboard_arrow_up),
+                  Icon(Icons.keyboard_arrow_up),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget cancelSlider() {
+    return Positioned(
+      right: -timerAnimation!.value,
+      child: Container(
+        height: widget.size!,
+        width: timerWidth,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(widget.radius!),
+          color: widget.color,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              showLottie ? const LottieAnimation() : Text(recordDuration),
+              SizedBox(width: widget.size!),
+              FlowShader(
+                child: Row(
+                  children: const [
+                    Icon(Icons.keyboard_arrow_left),
+                    Text("Slide to cancel")
+                  ],
+                ),
+                duration: const Duration(seconds: 3),
+                flowColors: const [Colors.white, Colors.grey],
+              ),
+              SizedBox(width: widget.size!),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget timerLocked() {
+    return Positioned(
+      right: 0,
+      child: Container(
+        height: widget.size!,
+        width: timerWidth,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(widget.radius==null?10: widget.radius!),
+          color: widget.color,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15, right: 25),
+          child: GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () async {
+              Vibrate.feedback(FeedbackType.success);
+              timer?.cancel();
+              timer = null;
+              startTime = null;
+              recordDuration = "00:00";
+
+              var filePath = await Record().stop();
+              // AudioState.files.add(filePath!);
+              // Globals.audioListKey.currentState!
+              //     .insertItem(AudioState.files.length - 1);
+              // debugPrint(filePath);
+
+              setState(() {
+                widget.filePath = filePath;
+                isLocked = false;
+
+                print(widget.filePath);
+              });
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Text(recordDuration),
+                FlowShader(
+                  duration: const Duration(seconds: 3),
+                  flowColors: const [Colors.white, Colors.grey],
+                  child: const Text("Tap lock to stop"),
+                ),
+                const Center(
+                  child: FaIcon(
+                    FontAwesomeIcons.lock,
+                    size: 18,
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget audioButton() {
+    return GestureDetector(
+      child: Transform.scale(
+        scale: buttonScaleAnimation!.value,
+        child: Container(
+          height: widget.size!,
+          width: widget.size!,
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Theme.of(context).primaryColor,
+          ),
+          child: const Icon(Icons.mic),
+        ),
+      ),
+      onLongPressDown: (_) {
+        debugPrint("onLongPressDown");
+        widget.controller.forward();
+      },
+      onLongPressEnd: (details) async {
+        debugPrint("onLongPressEnd");
+
+        if (isCancelled(details.localPosition, context)) {
+          Vibrate.feedback(FeedbackType.heavy);
+
+          timer?.cancel();
+          timer = null;
+          startTime = null;
+          recordDuration = "00:00";
+
+          setState(() {
+            showLottie = true;
+          });
+
+          Timer(const Duration(milliseconds: 1440), () async {
+            widget.controller.reverse();
+            debugPrint("Cancelled recording");
+            var filePath = await record!.stop();
+            debugPrint(filePath);
+            File(filePath!).delete();
+            debugPrint("Deleted $filePath");
+            showLottie = false;
+          });
+        } else if (checkIsLocked(details.localPosition)) {
+          widget.controller.reverse();
+
+          Vibrate.feedback(FeedbackType.heavy);
+          debugPrint("Locked recording");
+          debugPrint(details.localPosition.dy.toString());
+          setState(() {
+            isLocked = true;
+          });
+        } else {
+          widget.controller.reverse();
+
+          Vibrate.feedback(FeedbackType.success);
+
+          timer?.cancel();
+          timer = null;
+          startTime = null;
+          recordDuration = "00:00";
+
+          var filePath = await Record().stop();
+          setState(() {
+            widget.filePath = filePath;
+          });
+
+          debugPrint("fileeesss${widget.filePath}$filePath");
+        }
+      },
+      onLongPressCancel: () {
+        debugPrint("onLongPressCancel");
+        widget.controller.reverse();
+      },
+      onLongPress: () async {
+        debugPrint("onLongPress");
+        Vibrate.feedback(FeedbackType.success);
+        if (await Record().hasPermission()) {
+          record = Record();
+          await record!.start(
+            path: "${widget.documentPath}audio_${DateTime.now().millisecondsSinceEpoch}.acc",
+            encoder: AudioEncoder.aacLc,
+            bitRate: 128000,
+            samplingRate: 44100,
+          );
+          startTime = DateTime.now();
+          timer = Timer.periodic(const Duration(seconds: 1), (_) {
+            final minDur = DateTime.now().difference(startTime!).inMinutes;
+            final secDur = DateTime.now().difference(startTime!).inSeconds % 60;
+            String min = minDur < 10 ? "0$minDur" : minDur.toString();
+            String sec = secDur < 10 ? "0$secDur" : secDur.toString();
+            setState(() {
+              recordDuration = "$min:$sec";
+            });
+          });
+        }
+      },
+    );
+  }
+
+  bool checkIsLocked(Offset offset) {
+    return (offset.dy < -35);
+  }
+
+  bool isCancelled(Offset offset, BuildContext context) {
+    return (offset.dx < -(MediaQuery.of(context).size.width * 0.2));
+  }
+}
+
---
+++ test/social_media_audio_recorder_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:social_media_audio_recorder/social_media_audio_recorder.dart';
+
+void main() {
+  // test('adds one to input values', () {
+  //   final calculator = Calculator();
+  //   expect(calculator.addOne(2), 3);
+  //   expect(calculator.addOne(-7), -6);
+  //   expect(calculator.addOne(0), 1);
+  // });
+}
